04af4e255abf6a889e2871ac5e94782d10dc7a9d
Matthew Miskovsky <matt.miskovsky@gmail.com>
2022-11-28 21:46:55 -0500

    user can now click on a marker on the map to view the name of the truck

--- frontend/src/pages/Foodtrucks.js
+++ frontend/src/pages/Foodtrucks.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import {Map, GoogleApiWrapper} from 'google-maps-react'
+import React, {useState} from 'react';
+import {Map, GoogleApiWrapper, Marker, InfoWindow} from 'google-maps-react'
 
  
 function Foodtrucks(props) {
+
+    const [state, setState] = useState({
+      showingInforWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+    })
+
+    const onMarkerClick = (props, marker, e) => {
+      console.log(props)
+      setState({
+        selectedPlace: props.name,
+        activeMarker: marker,
+        showingInforWindow: !state.showingInforWindow
+      })
+    }
+
     return (
-      <Map
+    <Map
       google = {props.google}
       style = {{width: "50%", height: "50%"}}
       zoom = {10}
       initialCenter={{ lat: 42.886448, lng: -78.878372}}
-      />
+      >
+        <Marker 
+        name='Queen City Food Truck' 
+        position={{ lat: 42.886448, lng: -78.878372}} 
+        onClick={onMarkerClick}
+        />
+          <InfoWindow
+          marker={state.activeMarker}
+          visible={state.showingInforWindow}
+          >
+            <div>
+              <h1>{state.selectedPlace}</h1>
+            </div>
+          </InfoWindow>
+      </Map>
     );
 }
 
